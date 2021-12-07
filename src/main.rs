280bc3b18db56a129e2211b93e1e699233e84b6a
Steven Shaw <steven.shaw@sbs.com.au>
2021-12-07 18:17:23 +1000

    List DynamoDB tables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_dynamodb::{Client, Error};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
+
+    let config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&config);
+
+    let resp = client.list_tables().send().await?;
+
+    println!("Tables:");
+
+    let names = resp.table_names().unwrap_or_default();
+
+    for name in names {
+        println!("  {}", name);
+    }
+
+    println!("Found {} tables", names.len());
+
+    Ok(())
 }
