e620060ed14f5915e7313ce252f3eb12dd2b404d
Steven Shaw <steven.shaw@sbs.com.au>
2021-12-07 18:27:33 +1000

    Tweak default region and print

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
 use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_dynamodb::{Client, Error};
 
+const DEFAULT_REGION : &str = "ap-southeast-2";
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
-
+    let region_provider = RegionProviderChain::default_provider().or_else(DEFAULT_REGION);
+    let region = region_provider.region().await;
+    println!("region = {:#?}", region);
     let config = aws_config::from_env().region(region_provider).load().await;
     let client = Client::new(&config);
 
     let resp = client.list_tables().send().await?;
-
-    println!("Tables:");
-
     let names = resp.table_names().unwrap_or_default();
 
+    println!("Tables {{");
     for name in names {
         println!("  {}", name);
     }
+    println!("}}");
 
     println!("Found {} tables", names.len());
 
